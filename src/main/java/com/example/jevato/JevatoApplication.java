f9dd5f499d45cd0acae06a6464dd4e3dd3dd1e99
TinyProgrammer <kubagrowy@gamil.com>
2024-07-25 15:59:48 +0200

    initial commit

---
+++ src/main/java/com/example/jevato/JevatoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jevato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JevatoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JevatoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jevato/JevatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jevato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JevatoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
